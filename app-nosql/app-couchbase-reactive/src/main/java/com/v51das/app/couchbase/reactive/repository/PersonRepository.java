43ec186747631f85b9c0311fc3fe88de52405f90
Dobin <liudebin@aipark.com>
2021-11-08 17:53:30 +0800

    增加对couchbase的开发

--- app-nosql/app-couchbase-reactive/src/main/java/com/v51das/app/couchbase/reactive/repository/PersonRepository.java
+++ app-nosql/app-couchbase-reactive/src/main/java/com/v51das/app/couchbase/reactive/repository/PersonRepository.java
@@ -1,9 +1,13 @@
 package com.v51das.app.couchbase.reactive.repository;
 
 import com.v51das.app.couchbase.reactive.model.Person;
+import org.springframework.data.couchbase.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person, String> {
     List<Person> findByFirstName(String firstName);
+
+    @Query("SELECT META().id AS __id, META().cas AS  __cas, firtName, lastName, created FROM _default")
+    List<Person> findByRoutes(String routeName);
 }
